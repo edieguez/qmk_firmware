fea285dd09695a115e1f2fd2218248b1ce643ff2
edieguez <dieguezemmanuel@gmail.com>
2026-07-29 19:07:03 -0600

    Add Fn keys to Fn layer

--- keyboards/soflecat_macos/keymaps/default/keymap.c
+++ keyboards/soflecat_macos/keymaps/default/keymap.c
@@ -22,20 +22,9 @@ enum layers {
     _ADJUST,
 };
 
-// Number row: tap for the digit, double-tap for the matching F-key.
 enum {
-    TD_NUM_1 = 0,
-    TD_NUM_2,
-    TD_NUM_3,
-    TD_NUM_4,
-    TD_NUM_5,
-    TD_NUM_6,
-    TD_NUM_7,
-    TD_NUM_8,
-    TD_NUM_9,
-    TD_NUM_0,
     // Caps Lock on tap, Caps Word toggle on double-tap.
-    TD_CAPS,
+    TD_CAPS = 0,
 };
 
 // ACTION_TAP_DANCE_DOUBLE's second tap is sent via register_code16(), which
@@ -59,17 +48,7 @@ static void caps_dance_reset(tap_dance_state_t *state, void *user_data) {
 }
 
 tap_dance_action_t tap_dance_actions[] = {
-    [TD_NUM_1] = ACTION_TAP_DANCE_DOUBLE(KC_1, KC_F1),
-    [TD_NUM_2] = ACTION_TAP_DANCE_DOUBLE(KC_2, KC_F2),
-    [TD_NUM_3] = ACTION_TAP_DANCE_DOUBLE(KC_3, KC_F3),
-    [TD_NUM_4] = ACTION_TAP_DANCE_DOUBLE(KC_4, KC_F4),
-    [TD_NUM_5] = ACTION_TAP_DANCE_DOUBLE(KC_5, KC_F5),
-    [TD_NUM_6] = ACTION_TAP_DANCE_DOUBLE(KC_6, KC_F6),
-    [TD_NUM_7] = ACTION_TAP_DANCE_DOUBLE(KC_7, KC_F7),
-    [TD_NUM_8] = ACTION_TAP_DANCE_DOUBLE(KC_8, KC_F8),
-    [TD_NUM_9] = ACTION_TAP_DANCE_DOUBLE(KC_9, KC_F9),
-    [TD_NUM_0] = ACTION_TAP_DANCE_DOUBLE(KC_0, KC_F10),
-    [TD_CAPS]  = ACTION_TAP_DANCE_FN_ADVANCED(NULL, caps_dance_finished, caps_dance_reset),
+    [TD_CAPS] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, caps_dance_finished, caps_dance_reset),
 };
 
 // Shift+Backspace sends Delete on every layer, so a dedicated Del key isn't
@@ -96,7 +75,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /*
  * BASE
  * ,-----------------------------------------------------.                    ,-----------------------------------------------------.
- * | GEsc |  1/F1|  2/F2|  3/F3|  4/F4|  5/F5|                                 |  6/F6|  7/F7|  8/F8|  9/F9| 0/F10|   -  |
+ * | GEsc |   1  |   2  |   3  |   4  |   5  |                                 |   6  |   7  |   8  |   9  |   0  |   -  |
  * |------+------+------+------+------+------|                                |------+------+------+------+------+------|
  * | Tab  |   Q  |   W  |   E  |   R  |   T  |                                 |   Y  |   U  |   I  |   O  |   P  |  \   |
  * |------+------+------+------+------+------|                                |------+------+------+------+------+------|
@@ -108,7 +87,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                       `----------------------------------'              '------''---------------------------'
  */
     [_BASE] = LAYOUT(
-        QK_GESC,     TD(TD_NUM_1),  TD(TD_NUM_2),  TD(TD_NUM_3),  TD(TD_NUM_4),  TD(TD_NUM_5),                                  TD(TD_NUM_6),  TD(TD_NUM_7),  TD(TD_NUM_8),  TD(TD_NUM_9),  TD(TD_NUM_0),    KC_MINUS,
+        QK_GESC,     KC_1,          KC_2,          KC_3,          KC_4,          KC_5,                                          KC_6,          KC_7,          KC_8,          KC_9,          KC_0,            KC_MINUS,
         KC_TAB,      KC_Q,          KC_W,          KC_E,          KC_R,          KC_T,                                          KC_Y,          KC_U,          KC_I,          KC_O,          KC_P,            KC_BSLS,
         TD(TD_CAPS), LALT_T(KC_A),  LCTL_T(KC_S),  LGUI_T(KC_D),  LSFT_T(KC_F),  KC_G,                                          KC_H,          RSFT_T(KC_J),  RGUI_T(KC_K),  RCTL_T(KC_L),  RALT_T(KC_SCLN), KC_QUOTE,
         OSM(MOD_LSFT), KC_Z,      KC_X,          KC_C,          KC_V,          KC_B,    KC_MUTE,             KC_MPLY,        KC_N,          KC_M,          KC_COMMA,      KC_DOT,        KC_SLASH,        OSM(MOD_RSFT),
@@ -146,11 +125,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
 /*
- * FN -- F11/F12. Blank cells are transparent (same key as _BASE).
+ * FN -- F1-F12 on the number row (F11/F12 on T/Y). Blank cells are
+ * transparent (same key as _BASE).
  * ,-----------------------------------------------------.                    ,-----------------------------------------------------.
- * |      | F11  | F12  |      |      |      |                                 |      |      |      |      |      |      |
+ * |      |  F1  |  F2  |  F3  |  F4  |  F5  |                                 |  F6  |  F7  |  F8  |  F9  |  F10 |      |
  * |------+------+------+------+------+------|                                |------+------+------+------+------+------|
- * |      |      |      |      |      |      |                                 |      |      |      |      |      |      |
+ * |      |      |      |      |      |  F11 |                                 |  F12 |      |      |      |      |      |
  * |------+------+------+------+------+------|                                |------+------+------+------+------+------|
  * |      |      |      |      |      |      |-------.                 ,-------|      |      |      |      |      |      |
  * |------+------+------+------+------+------|       |                |       |------+------+------+------+------+------|
@@ -160,8 +140,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                       `----------------------------------'              '------''---------------------------'
  */
     [_FN] = LAYOUT(
-        KC_TRNS,  KC_F11,  KC_F12,  KC_TRNS, KC_TRNS, KC_TRNS,                                     KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                                     KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                                       KC_F6,   KC_F7,   KC_F8,    KC_F9,   KC_F10,  KC_TRNS,
+        KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_F11,                                       KC_F12,  KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS,
         KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                                     KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS,
         KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS,           KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS,
                                     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
--- keyboards/soflecat_macos/keymaps/default/rules.mk
+++ keyboards/soflecat_macos/keymaps/default/rules.mk
@@ -5,7 +5,9 @@ CAPS_WORD_ENABLE = yes
 KEY_OVERRIDE_ENABLE = yes
 
 # Real controller is a Pro Micro-footprint RP2040 clone, not the AVR Pro
-# Micro keyboard.json's development_board targets.
-CONVERT_TO = promicro_rp2040
+# Micro keyboard.json's development_board targets. `sparkfun_pm2040` is what
+# the older deprecated `promicro_rp2040` option aliased to internally, so this
+# is a like-for-like swap, not a hardware-behavior change.
+CONVERT_TO = sparkfun_pm2040
 
 SRC += lib/bongocat.c
